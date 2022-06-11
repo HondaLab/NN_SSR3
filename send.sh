95811063b27ba980eaa4be301883e116bcbe6ce5
Yasushi Honda <aeroboy08@gmail.com>
2022-06-11 14:02:01 +0900

    NNの学習結果をロボットに送信するためのスクリプト

--- send.sh
+++ send.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/expect
 
 set timeout 9
-spawn scp Input_data_max.csv Output_data_max.csv disdata_num_weight.csv history_data_step_num.csv optimum_weight_1000 optimum_weight_2000 pi@172.16.7.103:/home/pi/NN_SSR2/NN_ssr2_chainer
+spawn scp Input_data_max.csv Output_data_max.csv disdata_num_weight.csv history_data_step_num.csv optimum_weight_1000 optimum_weight_2000 pi@172.16.7.103:/home/pi/NN_SSR3
 expect "s password:"
 send "ssr2\n"
 interact
