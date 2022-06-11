69ab631c778cb4efbf4d82507ca7bc9a0f8e3cd4
Your Name <you@example.com>
2022-06-12 02:04:42 +0900

    NNの学習結果をロボットに送信するためのスクリプト

--- send.sh
+++ send.sh
@@ -1,7 +1,14 @@
-#!/usr/bin/expect
-
-set timeout 9
-spawn scp Input_data_max.csv Output_data_max.csv disdata_num_weight.csv history_data_step_num.csv optimum_weight_1000 optimum_weight_2000 pi@172.16.7.103:/home/pi/NN_SSR3
-expect "s password:"
-send "ssr2\n"
-interact
+#!/usr/bin/sh
+dest=pi@172.16.8.100:/home/pi/NN_SSR3
+file1=Input_data_max.csv
+file2=Output_data_max.csv
+file3=disdata_num_weight.csv
+file4=history_data_step_num.csv
+file5=optimum_weight_1000
+file6=optimum_weight_2000
+scp $file1 $dest
+scp $file2 $dest
+scp $file3 $dest
+scp $file4 $dest
+scp $file5 $dest
+scp $file6 $dest
